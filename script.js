5f86a7ff3a0d96c7970f51f2520487708c9490d0
FERNANDO <Fernando@FERNANDO.local>
2021-08-25 16:46:30 -0300

    valida senha requisito 3

---
+++ script.js
@@ -0,0 +1,14 @@
+const buttonEmail = document.getElementById('button-login');
+const loginEmail = document.getElementById('input-email');
+const passwordEmail = document.getElementById('input-password');
+const login = 'tryber@teste.com';
+const password = '123456';
+
+function verifyLogin() {
+  if (loginEmail.value === login && passwordEmail.value === password) {
+    alert('Olá Tryber');
+  } else {
+    alert('Email ou senha inválidos');
+  }
+}
+buttonEmail.addEventListener('click', verifyLogin);
